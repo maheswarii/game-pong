02b5ac7b5396d43c727b065ece1405ca70db0431
maheswarii <62419626+maheswarii@users.noreply.github.com>
2020-04-15 14:26:41 +0700

    update randomness and scoring

---
+++ Assets/Scripts/ballmovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmovement : MonoBehaviour
+{
+    //public int speed = 30;
+    public Rigidbody2D sesuatu;
+    public GameObject masterScript;
+    public Animator animtr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = Random.Range(0, 2) * 2 - 1; // nilai x bisa bernilai -1 atau 1
+        int y = Random.Range(0, 2) * 2 - 1; // nilai y bisa bernilai -1 atau 1
+        int speed = Random.Range(20, 26); // nilai speed antara 20 sampai 25
+        sesuatu.velocity = new Vector2(-1,-1) * speed;
+        sesuatu.GetComponent<Transform>().position = Vector2.zero;
+        animtr.SetBool("IsMove", true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(sesuatu.velocity.x > 0 ){ // bola bergerak kek kanan
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }else{
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.collider.name=="dindingKanan" || other.collider.name=="dindingKiri"){
+            masterScript.GetComponent<ScoringScript>().UpdateScore(other.collider.name);
+            StartCoroutine(jeda()); // untuk pindah ke tengah
+        }
+    }
+
+    IEnumerator jeda(){
+        sesuatu.velocity = Vector2.zero; // menghenntikan bola
+        animtr.SetBool("IsMove", false); // mengubah animasi ke api berhenti
+        sesuatu.GetComponent<Transform>().position = Vector2.zero; // mengubah posisi bola
+
+        yield return new WaitForSeconds(1);
+
+        int x = Random.Range(0, 2) * 2 - 1; // nilai x bisa bernilai -1 atau 1
+        int y = Random.Range(0, 2) * 2 - 1; // nilai y bisa bernilai -1 atau 1
+        int speed = Random.Range(20, 26); // nilai speed antara 20 sampai 25
+        sesuatu.velocity = new Vector2(x, y) * speed; // mengatur kecepatan
+        animtr.SetBool("IsMove", true); // mengubah animasi ke api bergerak
+    }
+}
